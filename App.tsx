9de8d63176f18236ed38f1c650b13048b4f2db1c
Yena-Yun <cheryleduart@gmail.com>
2023-10-30 15:44:17 +0900

    Start: RN(Expo) + TS + styled-components 시작

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StyledText>Open up App.tsx to start working on your app!</StyledText>
+      <StatusBar style='auto' />
     </View>
   );
 }
+
+const StyledText = styled.Text`
+  color: palevioletred;
+`;
 
 const styles = StyleSheet.create({
   container: {
